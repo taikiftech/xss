940ad163f17abe5e37d7d8b2d41e47ac328b5f66
taikif <hatakeyama.taiki1@gmail.com>
2023-06-15 10:47:06 +0900

    first commit

---
+++ src/test/java/com/example/xss/XssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
