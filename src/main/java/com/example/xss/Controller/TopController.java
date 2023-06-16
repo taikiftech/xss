d2a2fbcd2948eb2d442acdb76764c3533242e4d1
taikif <hatakeyama.taiki1@gmail.com>
2023-06-16 11:56:43 +0900

    make super super beginner done

--- src/main/java/com/example/xss/Controller/TopController.java
+++ src/main/java/com/example/xss/Controller/TopController.java
@@ -1,13 +1,28 @@
 package com.example.xss.Controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TopController {
     
     @GetMapping("/")
-    public String Top(){
+    public String TopGet(HttpServletResponse resp){
+        Cookie cokkie = new Cookie("flag","{Y0u_4re_h4cker_righ7?}");
+        cokkie.setMaxAge(3600);
+        resp.addCookie(cokkie);
         return "top";
     }
+
+    @PostMapping("/")
+    public String TopPost(){
+        return "top";
+    }
+
+    //@GetMapping()
+
 }
---
+++ src/main/resources/static/xss.js
@@ -0,0 +1,5 @@
+function input_form(){
+    $text = document.getElementById('text').value;
+    var title = document.getElementById('title');
+    title.innerHTML = $text;
+}
