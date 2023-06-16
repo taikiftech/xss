580a7fa1bca9ce62ca5b1f056eed50cd8edfd2d6
taikif <hatakeyama.taiki1@gmail.com>
2023-06-16 16:29:59 +0900

    topの修正

--- src/main/resources/static/xss.js
+++ src/main/resources/static/xss.js
@@ -1,5 +1,5 @@
 function input_form(){
     $text = document.getElementById('text').value;
     var title = document.getElementById('title');
-    title.innerHTML = "<div onclick=\"alert('jjj');\">aaa</div>";
+    title.innerHTML = $text;
 }
