81341ac5d539c157bf1110da99eef086ccac21a3
taikif <hatakeyama.taiki1@gmail.com>
2023-06-26 11:15:58 +0900

    改訂

--- src/main/resources/static/xss_lv2.js
+++ src/main/resources/static/xss_lv2.js
@@ -6,13 +6,13 @@ function xss_lv2(){
     const first = document.getElementById('first').value;
     var title = document.getElementById('title');
 
-    if (last.indexOf('script')!= -1 | -1 != last.indexOf('div')| -1 != last.indexOf('button')| -1 != last.indexOf('a')| -1 != last.indexOf('p')| -1 != last.indexOf('h')){
+    if (last.indexOf('script')!= -1 | -1 != last.indexOf('div')| -1 != last.indexOf('button')| -1 != last.indexOf('a')| -1 != last.indexOf('p')| -1 != last.indexOf('h')| -1 != last.indexOf('img')| -1 != last.indexOf('onerror')){
         alert('攻撃しないでください( *´艸｀)');
         return ;
-    }else if (-1 != middle.indexOf('script') | -1 != middle.indexOf('div') | -1 != last.indexOf('button')| -1 != last.indexOf('a')| -1 != last.indexOf('p')| -1 != last.indexOf('h')){
+    }else if (-1 != middle.indexOf('script') | -1 != middle.indexOf('div') | -1 != last.indexOf('button')| -1 != last.indexOf('a')| -1 != last.indexOf('p')| -1 != last.indexOf('h')| -1 != last.indexOf('img')| -1 != last.indexOf('onerror')){
         alert('攻撃しないでください( *´艸｀)');
         return ;
-    }else if (-1 != first.indexOf('script') | -1 != first.indexOf('div')| -1 != last.indexOf('button')| -1 != last.indexOf('a')| -1 != last.indexOf('p')| -1 != last.indexOf('h')){
+    }else if (-1 != first.indexOf('script') | -1 != first.indexOf('div')| -1 != last.indexOf('button')| -1 != last.indexOf('a')| -1 != last.indexOf('p')| -1 != last.indexOf('h')| -1 != last.indexOf('img')| -1 != last.indexOf('onerror')){
         alert('攻撃しないでください( *´艸｀)');
         return ;
     }else{
