c0d521cf1c7f3f8957403b2b0e6d0dd87243a065
taikif <hatakeyama.taiki1@gmail.com>
2023-06-26 15:05:30 +0900

    xss_lv3の完成

--- src/main/resources/static/xss_lv3.js
+++ src/main/resources/static/xss_lv3.js
@@ -1,8 +1,18 @@
 $("button").click(function(){
-    const text = document.getElementById('text').value;
-    $("ol").append("<li>"+text+"</li>");
+   const text = document.getElementById('text').value.toLowerCase();
+    if(text.indexOf('/div')!= -1 | text.indexOf('/img')!= -1 | text.indexOf('/a')!= -1 | text.indexOf('/p')!= -1 | text.indexOf('/button')!= -1 | text.indexOf('/h')!= -1 | text.indexOf('/form')!= -1 | text.indexOf('</') == -1){
+        alert('攻撃しないでください( *´艸｀)');
+        return ;
+    }
+   $("ol").append("<li>"+text+"</li>");
 });
 
-function xss_lv3(){
-    
-}
+// function xss_lv3(){
+//     var text = document.getElementById('text').value;
+//     text = document.createTextNode(text);
+//     var li = document.createElement('li');
+//     li.appendChild(text);
+
+//     var ol = document.getElementById('ol');
+//     ol.appendChild(li);
+// }
